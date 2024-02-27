4dbbad0fea8b0cec17d00d8b0265b57de819d22a
fatimabolotbek <zhumalieva91@gmail.com>
2024-02-27 21:00:12 +0000

    homeworks

---
+++ homework/session3h.py
@@ -0,0 +1,13 @@
+string1 = "<<"
+string2 = ">>"
+inserted_string = "hello" #  using the + operator, the join() method, the string() format method, and the f-string
+new_string = f"{string1}{inserted_string}{string2}"
+print(new_string)
+
+
+
+string1 = "aa"
+string2 = "bb"
+insterted_string = "hello"
+new_string = f"{string1}{inserted_string}{string2}"
+print(new_string)
