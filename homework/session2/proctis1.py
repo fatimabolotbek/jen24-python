2d9b8497305443d5c3573a6f129943863a306578
fatimabolotbek <zhumalieva91@gmail.com>
2024-03-02 21:13:16 +0000

    added file and changed the folder structure

---
+++ homework/session2/proctis1.py
@@ -0,0 +1,33 @@
+    
+import re
+while True:
+    password = input(" Enter your password:")
+    if len(password) < 8: 
+        print("Password muse be accepted 8 characters")
+    elif not re.search ('[A-Z]', password):
+        print("password muse be acceptet list one uppercase letter.")
+    elif not  re.search ('[a-z]', password):       
+        print ("password muse be acceptet list one lowercase letter")
+    elif not re.search ('[0-9]', password):
+        print("Password must contain at least one numbe")
+    elif not re.searc('[#@^%$^]', password):
+        print("Password must contain at least one special character")
+    else:
+        print ("password accepted")
+        break
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ session3.py
@@ -0,0 +1,16 @@
+#for i in range(5):
+ #   print(i)  
+#else:
+   # print("The loop is finish")
+     
+
+
+#for i in range(1, 4):  #(1, 4)
+     #for j in range(1, 3):   # (1, 5)
+       ## print(i, j)   #(i, j)
+
+        
+
+#for num in numbers[ 10 , 10.4 , 90. 110, 20]
+   # print num [sum]:
+jan24_names
